0eae71dda7f239b17dd0b6d56232b3c083a88d2f
Remco Bloemen <remco@wicked.ventures>
2024-10-01 10:52:21 +0200

    Prime Field

--- src/tr03111/elliptic_curve.rs
+++ src/tr03111/elliptic_curve.rs
@@ -1,5 +1,8 @@
 use {
-    super::{parse_uint, parse_uint_ref, EcParameters, PrimeField, ID_PRIME_FIELD},
+    super::{
+        parse_uint, parse_uint_ref, prime_field::PrimeFieldElement, EcParameters, PrimeField,
+        ID_PRIME_FIELD,
+    },
     anyhow::{bail, ensure, Result},
     der::asn1::IntRef,
     ruint::aliases::U320,
@@ -9,10 +12,12 @@ use {
 pub struct EllipticCurve {
     pub base_field: PrimeField,
     pub scalar_field: PrimeField,
-    pub a: U320,
-    pub b: U320,
+    // Curve parameters in Montgomery form.
+    // Ideally we would store as PrimeFieldElement, but that would require a reference to the field and confuses the borrow checker.
+    a_monty: U320,
+    b_monty: U320,
     pub cofactor: U320,
-    pub generator: (U320, U320),
+    generator_monty: (U320, U320),
 }
 
 impl EllipticCurve {
@@ -29,12 +34,12 @@ impl EllipticCurve {
         let modulus: IntRef = params.field_id.parameters.decode_as()?;
         let modulus = parse_uint_ref(modulus)?;
         // ensure!(is_prime(&modulus));
-        let base_field = PrimeField::new(modulus);
+        let base_field = PrimeField::from_modulus(modulus);
 
         let order = parse_uint(&params.order)?;
         // ensure!(is_prime(&order));
         ensure!(order != modulus);
-        let scalar_field = PrimeField::new(order);
+        let scalar_field = PrimeField::from_modulus(order);
 
         let cofactor = (params.cofactor)
             .as_ref()
@@ -46,32 +51,46 @@ impl EllipticCurve {
         let a = base_field.os2fe(params.curve.a.as_bytes());
         let b = base_field.os2fe(params.curve.b.as_bytes());
 
-        // Check non-singualr requirement 4a^3 + 27b^2 != 0
-        let mut check_a = base_field.pow(a, U320::from(3));
-        check_a = base_field.mul(check_a, U320::from(4));
-        let mut check_b = base_field.pow(b, U320::from(2));
-        check_b = base_field.mul(check_b, U320::from(27));
-        ensure!(base_field.add(check_a, check_b) != U320::ZERO);
+        // Check non-singular requirement 4a^3 + 27b^2 != 0
+        ensure!(
+            base_field.el_from_u64(4) * a.pow(3) + base_field.el_from_u64(27) * b.pow(2)
+                != base_field.zero()
+        );
 
         let mut curve = Self {
             base_field,
             scalar_field,
-            a,
-            b,
+            a_monty: a.as_uint_monty(),
+            b_monty: b.as_uint_monty(),
             cofactor,
-            generator: (U320::ZERO, U320::ZERO),
+            generator_monty: (U320::ZERO, U320::ZERO),
         };
-        curve.generator = curve.parse_point(params.base.as_bytes())?;
+        let generator = curve.parse_point(params.base.as_bytes())?;
+        curve.generator_monty = (generator.0.as_uint_monty(), generator.1.as_uint_monty());
         Ok(curve)
     }
 
-    pub fn ensure_valid(&self, (x, y): (U320, U320)) -> Result<()> {
-        let lhs = self.base_field.pow(y, U320::from(2));
-        let mut rhs = self.base_field.pow(x, U320::from(3));
-        rhs = self.base_field.add(rhs, self.base_field.mul(self.a, x));
-        rhs = self.base_field.add(rhs, self.b);
-        ensure!(lhs == rhs, "Point not on curve.");
+    pub fn a(&self) -> PrimeFieldElement {
+        self.base_field.el_from_monty(self.a_monty)
+    }
 
+    pub fn b(&self) -> PrimeFieldElement {
+        self.base_field.el_from_monty(self.b_monty)
+    }
+
+    pub fn generator(&self) -> (PrimeFieldElement, PrimeFieldElement) {
+        (
+            self.base_field.el_from_monty(self.generator_monty.0),
+            self.base_field.el_from_monty(self.generator_monty.1),
+        )
+    }
+
+    pub fn ensure_valid(&self, (x, y): (PrimeFieldElement, PrimeFieldElement)) -> Result<()> {
+        // Check curve equation y^2 = x^3 + ax + b
+        ensure!(
+            y.pow(2) == x.pow(3) + self.a() * x + self.b(),
+            "Point not on curve."
+        );
         if self.cofactor != U320::from(1) {
             // TODO: Check cofactor. self.order * point == infty
             bail!("Cofactor check unimplemented")
@@ -79,9 +98,9 @@ impl EllipticCurve {
         Ok(())
     }
 
-    pub fn parse_point(&self, bytes: &[u8]) -> Result<(U320, U320)> {
+    pub fn parse_point(&self, bytes: &[u8]) -> Result<(PrimeFieldElement, PrimeFieldElement)> {
         ensure!(!bytes.is_empty());
-        let fe_len = self.base_field.modulus.byte_len();
+        let fe_len = self.base_field.byte_len();
         let point = match bytes[0] {
             0x00 => unimplemented!("Point at infinity"),
             0x02 => unimplemented!("Compressed point with y'p = 0"),
--- src/tr03111/prime_field.rs
+++ src/tr03111/prime_field.rs
@@ -1,13 +1,89 @@
-use ruint::aliases::U320;
+use {
+    ruint::aliases::{U320, U64},
+    std::{
+        fmt::{self, Debug, Display, Formatter},
+        ops::{Add, AddAssign, Div, Mul, MulAssign, Neg},
+    },
+};
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
 pub struct PrimeField {
-    pub modulus: U320,
+    modulus: U320,
+
+    // Precomputed values for Montgomery multiplication.
+    montgomery_r: U320,  // R = 2^64*LIMBS mod modulus
+    montgomery_r2: U320, // R^2, or R in Montgomery form
+    montgomery_r3: U320, // R^3, or R^2 in Montgomery form
+    mod_inv: u64,        // -1 / modulus mod 2^64
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct PrimeFieldElement<'a> {
+    field: &'a PrimeField,
+    value: U320,
 }
 
 impl PrimeField {
-    pub fn new(modulus: U320) -> Self {
-        Self { modulus }
+    pub fn from_modulus(modulus: U320) -> Self {
+        assert_ne!(modulus, U320::ZERO);
+        let mod_inv = U64::wrapping_from(modulus)
+            .inv_ring()
+            .unwrap()
+            .wrapping_neg()
+            .to();
+        let montgomery_r = U320::from(2).pow_mod(U320::from(U320::BITS), modulus);
+        let montgomery_r2 = montgomery_r.mul_mod(montgomery_r, modulus);
+        let montgomery_r3 = montgomery_r2.mul_mod(montgomery_r, modulus);
+        Self {
+            modulus,
+            mod_inv,
+            montgomery_r,
+            montgomery_r2,
+            montgomery_r3,
+        }
+    }
+
+    pub fn modulus(&self) -> U320 {
+        self.modulus
+    }
+
+    pub fn byte_len(&self) -> usize {
+        self.modulus().byte_len()
+    }
+
+    pub fn zero(&self) -> PrimeFieldElement {
+        PrimeFieldElement {
+            field: self,
+            value: U320::ZERO,
+        }
+    }
+
+    pub fn one(&self) -> PrimeFieldElement {
+        PrimeFieldElement {
+            field: self,
+            value: self.montgomery_r,
+        }
+    }
+
+    #[inline]
+    pub fn el_from_u64(&self, value: u64) -> PrimeFieldElement {
+        self.el_from_uint(U320::from(value))
+    }
+
+    #[inline]
+    pub fn el_from_uint(&self, value: U320) -> PrimeFieldElement {
+        assert!(value < self.modulus);
+        // Convert to Montgomery form by multiplying with R.
+        PrimeFieldElement {
+            field: self,
+            value: self.mont_mul(value, self.montgomery_r2),
+        }
+    }
+
+    #[inline]
+    pub fn el_from_monty(&self, value: U320) -> PrimeFieldElement {
+        assert!(value < self.modulus);
+        PrimeFieldElement { field: self, value }
     }
 
     /// Implements TR-03111 section 3.1.3 OS2FE procedure.
@@ -18,57 +94,143 @@ impl PrimeField {
     /// allow it.
     ///
     /// The matching encoding procedure, however, does specify an exact length.
-    pub fn os2fe(&self, os: &[u8]) -> U320 {
+    pub fn os2fe(&self, os: &[u8]) -> PrimeFieldElement {
         if os.len() != self.modulus.byte_len() {
             eprintln!("Warning: OS2FE input length does not match modulus length")
         }
-
-        let mut result = U320::ZERO;
+        let mut result = self.zero();
+        let base = self.el_from_u64(256);
         for byte in os {
-            let byte = U320::from(*byte);
-            result = result.mul_mod(U320::from(256), self.modulus); // Need reduction here as it can overflow.
-            result = result.add_mod(byte, self.modulus);
+            result *= base;
+            result += self.el_from_u64(*byte as u64);
         }
         result
     }
 
-    pub fn add(&self, a: U320, b: U320) -> U320 {
-        assert!(a < self.modulus);
-        assert!(b < self.modulus);
-        a.add_mod(b, self.modulus)
+    /// Montogomery multiplication
+    #[inline]
+    fn mont_mul(&self, a: U320, b: U320) -> U320 {
+        a.mul_redc(b, self.modulus, self.mod_inv)
+    }
+}
+
+impl PrimeFieldElement<'_> {
+    pub fn field(&self) -> &PrimeField {
+        self.field
     }
 
-    pub fn sub(&self, a: U320, b: U320) -> U320 {
-        assert!(a < self.modulus);
-        assert!(b < self.modulus);
-        a.add_mod(self.neg(b), self.modulus)
+    #[inline]
+    pub fn to_uint(self) -> U320 {
+        self.field.mont_mul(self.value, U320::from(1))
     }
 
-    pub fn mul(&self, a: U320, b: U320) -> U320 {
-        assert!(a < self.modulus);
-        assert!(b < self.modulus);
-        a.mul_mod(b, self.modulus)
+    #[inline]
+    pub fn as_uint_monty(self) -> U320 {
+        self.value
     }
 
-    pub fn inv(&self, a: U320) -> Option<U320> {
-        assert!(a < self.modulus);
-        a.inv_mod(self.modulus)
+    /// Exponentiation
+    ///
+    /// Run time may depend on the exponent.
+    #[inline]
+    pub fn pow(self, exponent: u64) -> Self {
+        match exponent {
+            0 => self.field.one(),
+            1 => self,
+            2 => self * self,
+            3 => self * self * self,
+            n if n % 2 == 0 => (self * self).pow(n / 2),
+            n => self * self.pow(n - 1),
+        }
     }
 
-    pub fn div(&self, a: U320, b: U320) -> Option<U320> {
-        assert!(a < self.modulus);
-        assert!(b < self.modulus);
-        b.inv_mod(self.modulus)
-            .map(|inv_b| a.mul_mod(inv_b, self.modulus))
+    /// Inversion
+    ///
+    /// Run time may depend on the value.
+    #[inline]
+    pub fn inv(self) -> Option<Self> {
+        self.value
+            .inv_mod(self.field.modulus)
+            .map(|value| PrimeFieldElement {
+                field: self.field,
+                value: self.field.mont_mul(value, self.field.montgomery_r3),
+            })
     }
+}
 
-    pub fn neg(&self, a: U320) -> U320 {
-        assert!(a < self.modulus);
-        self.modulus - a
+impl Debug for PrimeFieldElement<'_> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        <U320 as Debug>::fmt(&self.to_uint(), f)
     }
+}
 
-    pub fn pow(&self, a: U320, b: U320) -> U320 {
-        assert!(a < self.modulus);
-        a.pow_mod(b, self.modulus)
+impl Display for PrimeFieldElement<'_> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        <U320 as Display>::fmt(&self.to_uint(), f)
+    }
+}
+
+impl Add for PrimeFieldElement<'_> {
+    type Output = Self;
+
+    #[inline]
+    fn add(self, other: Self) -> Self {
+        assert_eq!(self.field, other.field);
+        Self {
+            field: self.field,
+            value: self.value.add_mod(other.value, self.field.modulus),
+        }
+    }
+}
+
+impl Mul for PrimeFieldElement<'_> {
+    type Output = Self;
+
+    #[inline]
+    fn mul(self, other: Self) -> Self {
+        assert_eq!(self.field, other.field);
+        Self {
+            field: self.field,
+            value: self.field.mont_mul(self.value, other.value),
+        }
+    }
+}
+
+impl Neg for PrimeFieldElement<'_> {
+    type Output = Self;
+
+    #[inline]
+    fn neg(self) -> Self {
+        Self {
+            field: self.field,
+            value: self.field.modulus - self.value,
+        }
+    }
+}
+
+impl Div for PrimeFieldElement<'_> {
+    type Output = Option<Self>;
+
+    /// Division
+    ///
+    /// Run time may depend on the value of the divisor.
+    #[inline]
+    fn div(self, other: Self) -> Option<Self> {
+        assert_eq!(self.field, other.field);
+        other.inv().map(|inv| self * inv)
+    }
+}
+
+impl AddAssign for PrimeFieldElement<'_> {
+    #[inline]
+    fn add_assign(&mut self, other: Self) {
+        *self = *self + other;
+    }
+}
+
+impl MulAssign for PrimeFieldElement<'_> {
+    #[inline]
+    fn mul_assign(&mut self, other: Self) {
+        *self = *self * other;
     }
 }
